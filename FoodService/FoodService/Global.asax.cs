72e205c0370ded3af156bd6d3c38c8bdef4f8537
alex415894 <alex415894@gmail.com>
2015-03-17 16:12:24 +0200

    Create new repository
    Create new repository

---
+++ FoodService/FoodService/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using FoodService.Models;
+using RolesIdentityApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace FoodService
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+           // Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ FoodService/FoodService/Migrations/Configuration.cs
@@ -0,0 +1,59 @@
+namespace FoodService.Migrations
+{
+    using FoodService.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FoodService.Models.ApplicationDbContext>
+    {
+
+
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "FoodService.Models.ApplicationDbContext";
+        }
+        bool AddUserAndRole(FoodService.Models.ApplicationDbContext context)
+        {
+            IdentityResult ir;
+            var rm = new RoleManager<IdentityRole>
+                (new RoleStore<IdentityRole>(context));
+            ir = rm.Create(new IdentityRole("canEdit"));
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            var user = new ApplicationUser()
+            {
+                UserName = "user1@contoso.com",
+            };
+            ir = um.Create(user, "P_assw0rd1");
+            if (ir.Succeeded == false)
+                return ir.Succeeded;
+            ir = um.AddToRole(user.Id, "canEdit");
+            return ir.Succeeded;
+        }
+
+
+        protected override void Seed(FoodService.Models.ApplicationDbContext context)
+        {
+            AddUserAndRole(context);
+
+
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
