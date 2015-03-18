eeece3288e88eccf26f8f3ae95cc0a17469140df
alex415894 <alex415894@gmail.com>
2015-03-18 14:21:50 +0200

    model
    model

---
+++ FoodService/FoodService/Controllers/api/MenuItemController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FoodService.Models;
+
+namespace FoodService.Controllers.api
+{
+    public class MenuItemController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/MenuItem
+        public IQueryable<MenuItem> GetMenuItems()
+        {
+            return db.MenuItem;
+        }
+
+        // GET: api/MenuItem/5
+        [ResponseType(typeof(MenuItem))]
+        public IHttpActionResult GetMenuItem(int id)
+        {
+            MenuItem menuItem = db.MenuItem.Find(id);
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(menuItem);
+        }
+
+        // PUT: api/MenuItem/5
+ 
+  /*      [ResponseType(typeof(void))]
+        public IHttpActionResult PutMenuItem(int id, MenuItem menuItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != menuItem.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(menuItem).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MenuItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+*/
+        // POST: api/MenuItem
+        [ResponseType(typeof(MenuItem))]
+        public IHttpActionResult PostMenuItem(MenuItem menuItem)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.MenuItem.Add(menuItem);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = menuItem.Id }, menuItem);
+        }
+
+        // DELETE: api/MenuItem/5
+        [ResponseType(typeof(MenuItem))]
+        public IHttpActionResult DeleteMenuItem(int id)
+        {
+            MenuItem menuItem = db.MenuItem.Find(id);
+            if (menuItem == null)
+            {
+                return NotFound();
+            }
+
+            db.MenuItem.Remove(menuItem);
+            db.SaveChanges();
+
+            return Ok(menuItem);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MenuItemExists(int id)
+        {
+            return db.MenuItem.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ FoodService/FoodService/Models/MenuItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodService.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int DishId { get; set; }
+
+        public Dish Dish { get; set; }
+    }
+}
